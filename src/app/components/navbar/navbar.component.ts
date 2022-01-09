f89196f9b82ba425a98dcfa9b9a02179feb07a38
Alessio De Vincentis <Alessio.DeVincentis@nttdata.com>
2022-01-10 00:05:02 +0100

    Menubar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  items: MenuItem[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label:'Dashboard',
+        icon:'pi pi-fw pi-home'
+      },
+      {
+        label:'Anagrafica',
+        icon:'pi pi-fw pi-users',
+        items:[
+          {
+            label:'Amministratori'
+
+          },
+          {
+            label:'Condomìni'
+
+          },
+          {
+            label:'Fornitori'
+          }
+        ]
+      },
+      {
+        label:'Pagamenti',
+        icon:'pi pi-fw pi-calendar',
+        items:[
+          {
+            label:'Edit',
+            icon:'pi pi-fw pi-pencil',
+            items:[
+              {
+                label:'Save',
+                icon:'pi pi-fw pi-calendar-plus'
+              },
+              {
+                label:'Delete',
+                icon:'pi pi-fw pi-calendar-minus'
+              },
+
+            ]
+          },
+          {
+            label:'Archieve',
+            icon:'pi pi-fw pi-calendar-times',
+            items:[
+              {
+                label:'Remove',
+                icon:'pi pi-fw pi-calendar-minus'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label:'Preventivo e Consuntivo',
+        icon:'pi pi-fw pi-list',
+        items:[
+          {
+            label:'Remove',
+            icon:'pi pi-fw pi-calendar-minus'
+          }
+        ]
+      },
+      {
+        label:'Reportistica',
+        icon:'pi pi-fw pi-print',
+        items:[
+          {
+            label:'Remove',
+            icon:'pi pi-fw pi-calendar-minus'
+          }
+        ]
+      }
+    ];
   }
 
 }
